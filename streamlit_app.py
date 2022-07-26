b7f1310d5ec8c8857c47537e8d82bd7e64f1650e
erikweller <erikgweller@gmail.com>
2022-07-25 20:20:25 -0400

    Updated try except logic for text input fields big wrap

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,31 +124,33 @@ except ValueError:
     print('Enter target ABV')
 
 
+try:
 
-weaker_req = stronger_abv - required_abv_abv
-stronger_req = required_abv_abv - weaker_abv
+    weaker_req = stronger_abv - required_abv_abv
+    stronger_req = required_abv_abv - weaker_abv
 
-required_stronger = round((stronger_req / (stronger_req + weaker_req)), sigfigs=4)
-required_weaker = round((weaker_req / (stronger_req + weaker_req)), sigfigs=4)
-percent_total_volume = round(quantity / required_stronger, sigfigs=3)
-weaker_total_volume = percent_total_volume * required_weaker
-total_oz = percent_total_volume * 33.814
-cases = total_oz / 240
+    required_stronger = round((stronger_req / (stronger_req + weaker_req)), sigfigs=4)
+    required_weaker = round((weaker_req / (stronger_req + weaker_req)), sigfigs=4)
+    percent_total_volume = round(quantity / required_stronger, sigfigs=3)
+    weaker_total_volume = percent_total_volume * required_weaker
+    total_oz = percent_total_volume * 33.814
+    cases = total_oz / 240
 
-if curr == 'USD':
-    price = round(price, sigfigs = 8)
-    curr_unit = 'USD'
-elif curr == 'EUR':
-    c = CurrencyRates()
-    rate = float(c.get_rate('EUR', 'USD'))
-    price = round(price * rate * 1.01, sigfigs=8)
-    curr_unit = 'EUR'
-elif curr == 'GBP':
-    c = CurrencyRates()
-    rate = float(c.get_rate('GBP', 'USD'))
-    price = round(price * rate * 1.01, sigfigs=8)
-    curr_unit = 'GBP'
-
+    if curr == 'USD':
+        price = round(price, sigfigs = 8)
+        curr_unit = 'USD'
+    elif curr == 'EUR':
+        c = CurrencyRates()
+        rate = float(c.get_rate('EUR', 'USD'))
+        price = round(price * rate * 1.01, sigfigs=8)
+        curr_unit = 'EUR'
+    elif curr == 'GBP':
+        c = CurrencyRates()
+        rate = float(c.get_rate('GBP', 'USD'))
+        price = round(price * rate * 1.01, sigfigs=8)
+        curr_unit = 'GBP'
+except ValueError:
+    print('')
 st.text('\n\n')
 st.markdown("""---""")
 
